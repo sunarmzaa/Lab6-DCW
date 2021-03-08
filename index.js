601892147c433eb6ed5dfbb55beeeab5418173c6
sunarmzaa <sunarmzaa@gmail.com>
2021-03-08 21:52:06 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+let express = require('express')
+let bodyParser = require('body-parser');
+let app = express()
+let router = express.Router()
+const PORT = 80
+
+let bears = {
+    list: [
+        { id: 1, name: "Winny", weight: 50 },
+        { id: 2, name: "Pooh", weight: 60 }]
+}
+//npm init --y
+//npm i -s express
+//npm i -s body-parser
+
+app.use('/api', bodyParser.json(), router);   //[use json]
+app.use('/api', bodyParser.urlencoded({ extended: false }), router);
+
+//all bears
+router.route('/bears')
+    .get((req, res) => res.json(bears.list))
+    .post((req, res) => {           //ดึงข้อมูลและเพิ่มข้อมูลได้ในฐานข้อมูล
+        let id = (bears.list.length) ? bears.list[bears.list.length - 1].id + 1 : 1
+        let name = req.body.name
+        let weight = req.body.weight
+        bears = { list: [...bears.list, { id, name, weight }] }
+        res.json(bears.list)
+    })
+
+//each bear    
+router.route('/bears/:bears_id')
+    .get((req, res) => {
+        let id = bears.list.findIndex((item) => (item.id == +req.params.bears_id))
+        res.json(bears.list[id])
+    })
+
+    //http://localhost/api/bears/...
+    //อัพเดตค่า
+    .put((req, res) => {
+        let id = bears.list.findIndex((item) => (item.id == +req.params.bears_id))
+        bears.list[id].name = req.body.name
+        bears.list[id].weight = req.body.weight
+        res.json(bears.list)
+
+    })
+    //ลบค่า
+    .delete((req, res) => {
+        bears.list = bears.list.filter(item => item.id !== +req.params.bears_id)
+        res.json(bears.list)
+    })
+
+app.listen(PORT, () => console.log('Server is running at ', PORT))
---
+++ index1.js
@@ -0,0 +1,56 @@
+let express = require('express')
+let bodyParser = require('body-parser');
+let app = express()
+let router = express.Router()
+const PORT = 80
+
+let people = {
+    list: [
+        { id: 111, name: "Warodom", surname: "Werapun" , major: "CoE" , gpa: "3.3" },
+        { id: 112, name: "John", surname: "Lennon" , major: "SE" , gpa: "2.87"}]
+}
+//npm init --y
+//npm i -s express
+//npm i -s body-parser
+
+app.use('/api', bodyParser.json(), router);   //[use json]
+app.use('/api', bodyParser.urlencoded({ extended: false }), router);
+
+//all bears
+router.route('/people')
+    .get((req, res) => res.json(people.list))
+    .post((req, res) => {           //ดึงข้อมูลและเพิ่มข้อมูลได้ในฐานข้อมูล
+        let id = (people.list.length) ? people.list[people.list.length - 1].id + 1 : 1
+        let name = req.body.name
+        let surname = req.body.surname
+        let major = req.body.major
+        let gpa = req.body.gpa
+        people = { list: [...people.list, { id, name, surname, major, gpa }] }
+        res.json(people.list)
+    })
+
+//each bear    
+router.route('/people/:people_id')
+    .get((req, res) => {
+        let id = people.list.findIndex((item) => (item.id == +req.params.people_id))
+        res.json(people.list[id])
+    })
+
+    //http://localhost/api/bears/...
+    //อัพเดตค่า
+    .put((req, res) => {
+        let id = people.list.findIndex((item) => (item.id == +req.params.people_id))
+        people.list[id].name = req.body.name
+        people.list[id].surname = req.body.surname
+        people.list[id].major = req.body.major
+        people.list[id].gpa = req.body.gpa
+        res.json(people.list)
+
+    })
+    //ลบค่า
+    .delete((req, res) => {
+        people.list = people.list.filter(item => item.id !== +req.params.people_id)
+        res.json(people.list)
+    })
+
+app.listen(PORT, () => console.log('Server is running at ', PORT))
